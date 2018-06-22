631e3a892b3ec114cec2b38f71e62233b69b5143
Ted Leahy <tedleahy28@gmail.com>
2018-06-22 14:00:44 +0100

    Allow users to update the amountSold value for any gem and save it to the gems JSON file

--- js/viewGem.js
+++ js/viewGem.js
@@ -1,12 +1,42 @@
 const {ipcRenderer} = require('electron')
 
+let gem
+
+function changeAmountSold(action, amountSoldEl) {
+    let newAmount = Number(amountSoldEl.innerHTML)
+
+    switch(action) {
+        case "increment":
+            newAmount++;
+            break;
+        case "decrement":
+            if (newAmount > 0) newAmount--;
+            break;
+    }
+    
+    amountSoldEl.innerHTML = newAmount
+    gem.amountSold = newAmount
+}
+
 ipcRenderer.on('gem-details', (event, args) => {
-    const gem = args[0]
+    gem = args[0]
     
     document.title = gem.name
     
     document.getElementById('header').innerHTML = gem.name
     document.getElementById('name').innerHTML = gem.name
     document.getElementById('price').innerHTML = gem.price
-    document.getElementById('amountSold').innerHTML = gem.amountSold
+    
+    let amountSold = document.getElementById('amountSold')
+    amountSold.innerHTML = gem.amountSold
+
+    let buttonEls = document.getElementsByClassName('changeAmountSold')
+    Array.from(buttonEls).forEach((btn) => {
+        btn.onclick = () => changeAmountSold(btn.dataset.action, amountSold)
+    })
 })
+
+document.getElementById('save-btn').onclick = () => {
+    amountSold = Number(document.getElementById('amountSold').innerHTML)
+    ipcRenderer.send('update-amount-sold', { id: gem.id, amountSold: gem.amountSold })
+}
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const {app, BrowserWindow, ipcMain} = require('electron')
 const fs = require('fs')
 
+const gemsFile = 'gems.json'
 let mainWindow, gemWindow
 
 // Read gems data from JSON file
-let gemFileData = fs.readFileSync('gems.json')
+let gemFileData = fs.readFileSync(gemsFile)
 let gems = JSON.parse(gemFileData)
 
 function createMainWindow() {
@@ -37,3 +38,9 @@ app.on('ready', createMainWindow)
 ipcMain.on('gems-request-sync', (event) => event.returnValue = gems)
 
 ipcMain.on('view-gem', (event, gemId) => createGemWindow(gemId))
+
+ipcMain.on('update-amount-sold', (event, args) => {
+    gems.map((gem) => { if (gem.id === args.id) gem.amountSold = args.amountSold })
+    
+    fs.writeFile(gemsFile, JSON.stringify(gems, null, 2))
+})
