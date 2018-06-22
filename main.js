ec1558c5c5fef9a171324e37dd3036676d719067
Ted Leahy <tedleahy28@gmail.com>
2018-06-22 02:48:31 +0100

    Display all existing gems in a table in the main window of the app

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const {app, BrowserWindow, ipcMain} = require('electron')
+const fs = require('fs')
 
 let mainWindow
 
 function createMainWindow() {
     // Create main app window
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({width: 1200, height: 900})
     mainWindow.loadFile('index.html')
 
     mainWindow.webContents.openDevTools()
 
-    mainWindow.on('closed', () => {
-        mainWindow = null // Dereference object
-    })
+    mainWindow.on('closed', () => mainWindow = null) // Dereference object
 }
 
 app.on('ready', createMainWindow)
+
+let gemFileData = fs.readFileSync('gems.json')
+let gems = JSON.parse(gemFileData)
+
+ipcMain.on('gems-request-sync', (event, arg) => event.returnValue = gems)
